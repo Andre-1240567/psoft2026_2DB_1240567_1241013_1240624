112097ad923e92b9921350b4257345f705a568dd
Andre-1240567 <1240567@isep.ipp.pt>
2026-04-03 20:10:51 +0100

    Initial project setup with Maven and Spring (Using Java21)

---
+++ src/main/java/pt/isep/psoft/alsafe/Psoft20262Db124056712410131240624Application.java
@@ -0,0 +1,13 @@
+package pt.isep.psoft.alsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psoft20262Db124056712410131240624Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Psoft20262Db124056712410131240624Application.class, args);
+    }
+
+}
---
+++ src/test/java/pt/isep/psoft/alsafe/Psoft20262Db124056712410131240624ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isep.psoft.alsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Psoft20262Db124056712410131240624ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
