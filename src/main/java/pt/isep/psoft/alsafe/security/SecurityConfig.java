d1989fc03833e751d3f3b7d2c814abba33eda328
AfonsoSobreiroISEP <1241013@isep.ipp.pt>
2026-06-09 11:43:12 +0100

    Small change on securityconfig

--- src/main/java/pt/isep/psoft/alsafe/security/SecurityConfig.java
+++ src/main/java/pt/isep/psoft/alsafe/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
