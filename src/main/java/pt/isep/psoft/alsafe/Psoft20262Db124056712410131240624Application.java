a2635f3ef89b64fab46bccd166b43ccf776551ad
José Alves <1240624@isep.ipp.pt>
2026-06-19 00:44:54 +0100

    Fix SpringBootApplication annotation syntax

--- src/main/java/pt/isep/psoft/alsafe/Psoft20262Db124056712410131240624Application.java
+++ src/main/java/pt/isep/psoft/alsafe/Psoft20262Db124056712410131240624Application.java
@@ -2,9 +2,10 @@ package pt.isep.psoft.alsafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(excludeName = { "org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration" })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Psoft20262Db124056712410131240624Application {
 
     public static void main(String[] args) {
